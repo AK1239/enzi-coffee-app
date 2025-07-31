0c33c18b7471fc934c81f764738b76f3ab3ae3dc
AK1239 <akilkhatri235@gmail.com>
2025-07-31 23:13:35 +0300

    chore: remove dist files from tracking and add to .gitignore

--- backend/dist/app.d.ts
+++
@@ -1,3 +0,0 @@
-import { Express } from 'express';
-export declare function createServer(): Express;
-//# sourceMappingURL=app.d.ts.map
--- backend/dist/utils/helpers.d.ts
+++
@@ -1,5 +0,0 @@
-import { Order } from '@/types';
-export declare const formatCurrency: (amount: number) => string;
-export declare const validateEmail: (email: string) => boolean;
-export declare const calculateOrderTotal: (items: Order["items"]) => number;
-//# sourceMappingURL=helpers.d.ts.map
